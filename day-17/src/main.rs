8b2d0121e0601e9544adf45034cddebf4a3c74cb
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2023-12-19 17:50:40 -0500

    complex

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -1,4 +1,8 @@
+use indexmap::IndexMap;
+use num_complex::Complex;
+use std::cmp::{Ordering, Reverse};
 use std::collections::{BinaryHeap, HashSet};
+use std::ops::RangeInclusive;
 
 
 #[cfg(feature = "sample")]
@@ -9,193 +13,145 @@ const DATA: &str = include_str!("../input.txt");
 #[cfg(not(feature = "part2"))]
 const LEGAL_CONSECUTIVE_MOVES: LegalMoves = LegalMoves { min: 1, max: 3 };
 #[cfg(feature = "part2")]
-const LEGAL_CONSECUTIVE_MOVES: LegalMoves = LegalMoves { min: 4, max: 7 };
+const LEGAL_CONSECUTIVE_MOVES: LegalMoves = LegalMoves { min: 4, max: 10 };
 
 pub struct LegalMoves {
-  pub min: usize,
-  pub max: usize,
+  pub min: i32,
+  pub max: i32,
 }
 
-#[derive(Debug)]
-struct Path {
-  y: usize,
-  x: usize,
-  dir: Direction,
-  heat_loss: u32,
-}
-
-impl Ord for Path {
-  fn cmp(&self, other: &Path) -> std::cmp::Ordering {
-    self.heat_loss.cmp(&other.heat_loss)
+impl LegalMoves {
+  pub fn range(&self) -> RangeInclusive<i32> {
+    self.min..=self.max
   }
 }
 
-impl PartialOrd for Path {
-  fn partial_cmp(&self, other: &Path) -> Option<std::cmp::Ordering> {
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+struct ComplexWrapper(Complex<i32>);
+
+impl ComplexWrapper {
+  fn magnitude(&self) -> i32 {
+    (self.0.norm_sqr() as f64).sqrt() as i32
+  }
+}
+
+impl PartialOrd for ComplexWrapper {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
     Some(self.cmp(other))
   }
 }
 
-impl PartialEq for Path {
-  fn eq(&self, other: &Path) -> bool {
-    self.heat_loss == other.heat_loss
-  }
-}
+impl Ord for ComplexWrapper {
+  fn cmp(&self, other: &Self) -> Ordering {
+    let self_mag = self.magnitude();
+    let other_mag = other.magnitude();
 
-impl Eq for Path {}
-
-#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Direction {
-  #[default]
-  #[allow(clippy::enum_variant_names)]
-  NoDirection,
-  Right,
-  Down,
-  Left,
-  Up,
-}
-
-impl Direction {
-  pub fn get_directed_neighbors(
-    (y, x): (usize, usize),
-    rows: usize,
-    cols: usize,
-  ) -> Vec<((usize, usize), Direction)> {
-    let mut neighbors = Vec::new();
-
-    for i in LEGAL_CONSECUTIVE_MOVES.min..=LEGAL_CONSECUTIVE_MOVES.max {
-      if x >= i {
-        neighbors.push(((y, x - i), Direction::Left));
-      }
-      if y + i < rows {
-        neighbors.push(((y + i, x), Direction::Down));
-      }
-      if x + i < cols {
-        neighbors.push(((y, x + i), Direction::Right));
-      }
-      if y >= i {
-        neighbors.push(((y - i, x), Direction::Up));
-      }
-    }
-
-    neighbors
-  }
-
-  pub fn invert(direction: &Direction) -> Direction {
-    match direction {
-      Direction::NoDirection => Direction::NoDirection,
-      Direction::Right => Direction::Left,
-      Direction::Down => Direction::Up,
-      Direction::Left => Direction::Right,
-      Direction::Up => Direction::Down,
-    }
+    self_mag
+      .partial_cmp(&other_mag)
+      .unwrap_or(Ordering::Equal)
+      .then_with(|| self.0.re.cmp(&other.0.re))
+      .then_with(|| self.0.im.cmp(&other.0.im))
   }
 }
 
 struct ProblemDefinition {
-  start: (usize, usize),
-  end: (usize, usize),
-  rows: usize,
-  cols: usize,
-  grid: Vec<Vec<u32>>,
+  grid: IndexMap<ComplexWrapper, i32>,
 }
-type Consequent = u32;
 
+type Consequent = i32;
 
 fn src_provider() -> Result<String, String> {
   Ok(DATA.to_string())
 }
 
-fn extract() -> Result<ProblemDefinition, String> {
-  let s = src_provider()?;
-  if s.chars().any(|c| !(c.is_ascii_digit() || c == '\n')) {
-    return Err("non digit in map!".to_string());
-  }
-
-  let grid: Vec<Vec<u32>> = s
+fn extract(data: &str) -> Result<ProblemDefinition, String> {
+  let grid: IndexMap<ComplexWrapper, i32> = data
     .lines()
-    .map(|l| {
-      l.chars()
-        .map(|c| c.to_digit(10).unwrap())
-        .collect::<Vec<_>>()
+    .enumerate()
+    .flat_map(|(i, row)| {
+      row.chars().enumerate().map(move |(j, c)| {
+        (
+          ComplexWrapper(Complex::new(i as i32, j as i32)),
+          c.to_digit(10).unwrap() as i32,
+        )
+      })
     })
-    .collect::<Vec<_>>();
-  let rows = grid.len();
-  let cols = grid.get(0).ok_or("no data in input")?.len();
-  let start = (0, 0);
-  let end = (rows - 1, cols - 1);
+    .collect();
 
-  Ok(ProblemDefinition { start, end, grid, rows, cols })
+  Ok(ProblemDefinition { grid })
 }
 
-fn get_legal_paths(
-  y: usize,
-  x: usize,
-  data: &ProblemDefinition,
-  previous_direction: Direction,
-  current_heat_loss: u32,
-) -> Vec<Path> {
-  Direction::get_directed_neighbors((y, x), data.rows, data.cols)
-    .into_iter()
-    .filter_map(|((next_y, next_x), dir)| {
-      if previous_direction == dir
-        || previous_direction == Direction::invert(&dir)
-      {
-        None
-      } else {
-        let mut current_coord = (y, x);
-        let inital_loss = data.grid[current_coord.0][current_coord.1];
-        let mut accumulated_loss = 0;
+fn find_shortest_path(
+  end_point: ComplexWrapper,
+  grid: &IndexMap<ComplexWrapper, i32>,
+) -> Option<i32> {
+  let one_imaginary = Complex::new(0, 1);
+  let mut todo: BinaryHeap<Reverse<(i32, ComplexWrapper, ComplexWrapper)>> =
+    BinaryHeap::new();
+  // Starting from position (0, 0) with an initial direction of moving right and down
+  todo.push(Reverse((
+    0,
+    ComplexWrapper(Complex::new(0, 0)),
+    ComplexWrapper(Complex::new(0, 1)),
+  )));
+  todo.push(Reverse((
+    0,
+    ComplexWrapper(Complex::new(0, 0)),
+    ComplexWrapper(Complex::new(1, 0)),
+  )));
 
-        while current_coord != (next_y, next_x) {
-          accumulated_loss += data.grid[current_coord.0][current_coord.1];
 
-          current_coord = match dir {
-            Direction::Left => (current_coord.0, current_coord.1 - 1),
-            Direction::Right => (current_coord.0, current_coord.1 + 1),
-            Direction::Up => (current_coord.0 - 1, current_coord.1),
-            Direction::Down => (current_coord.0 + 1, current_coord.1),
-            Direction::NoDirection => unreachable!(), // Shouldn't happen
-          };
+  let mut visited = HashSet::new();
+
+  while let Some(Reverse((value, position, direction))) = todo.pop() {
+    if position == end_point {
+      return Some(value);
+    }
+
+    if visited.contains(&(position, direction)) {
+      continue;
+    }
+
+    visited.insert((position, direction));
+
+    for delta in [one_imaginary / direction.0, -one_imaginary / direction.0] {
+      for steps in LEGAL_CONSECUTIVE_MOVES.range() {
+        let new_position = position.0 + delta * steps;
+        // eprintln!(
+        //   "Debug Info: Current position: (y{},x{}), direction: (y{},x{}), \
+        //    Delta: {delta}, Steps: {steps}",
+        //   position.0.re, position.0.im, direction.0.re, direction.0.im
+        // );
+
+        if grid.get(&ComplexWrapper(new_position)).is_some() {
+          let step_values: i32 = (1..=steps)
+            .map(|j| grid.get(&ComplexWrapper(position.0 + delta * j)).unwrap())
+            .sum();
+          todo.push(Reverse((
+            value + step_values,
+            ComplexWrapper(new_position),
+            ComplexWrapper(delta),
+          )));
         }
-        accumulated_loss -= inital_loss;
-        let heat_loss = current_heat_loss + accumulated_loss;
-        Some(Path { y: next_y, x: next_x, dir, heat_loss })
       }
-    })
-    .collect::<Vec<_>>()
+    }
+  }
+
+  None
 }
 
 fn transform(data: ProblemDefinition) -> Result<Consequent, String> {
-  let mut paths: BinaryHeap<Path> = [Direction::Right, Direction::Down]
-    .into_iter()
-    .map(|dir| Path { y: data.start.0, x: data.start.1, dir, heat_loss: 0 })
-    .collect();
+  let end_point = data.grid.last().ok_or("Empty map")?.0;
 
-  let mut visited: HashSet<usize> = HashSet::new();
-
-  loop {
-    if let Some(Path { x, y, dir, heat_loss }) = paths.pop() {
-      dbg!(((y, x), dir, heat_loss), &paths);
-      if !visited.contains(&(2 * (y * data.cols + x) + (dir as usize % 2))) {
-        if (y, x) == data.end {
-          return Ok(heat_loss);
-        } else {
-          let previous_direction = dir;
-          visited.insert(2 * (y * data.cols + x) + (dir as usize % 2));
-          paths.extend(get_legal_paths(
-            y,
-            x,
-            &data,
-            previous_direction,
-            heat_loss,
-          ));
-        }
-      }
-    } else {
-      return Err("No path found!".to_string());
-    }
+  // Check if end_point is within the bounds of the grid
+  if !data.grid.contains_key(end_point) {
+    return Err("End point is not in the grid".to_string());
   }
+
+  let heat_loss =
+    find_shortest_path(*end_point, &mut &data.grid).ok_or("no path found!")?;
+
+  Ok(heat_loss)
 }
 
 fn load(result: Result<Consequent, String>) -> Result<(), String> {
@@ -208,7 +164,8 @@ fn load(result: Result<Consequent, String>) -> Result<(), String> {
 }
 
 fn main() -> Result<(), String> {
-  let data = extract()?;
+  let data_str = src_provider()?;
+  let data = extract(&data_str)?;
   let result = transform(data);
 
   load(result)
